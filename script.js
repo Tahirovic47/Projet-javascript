193e2dd20beb203f9ddc2a276f92db3a63d5c17a
Tahir <mohammeed.tahir@gmail.com>
2024-04-07 08:37:09 -0400

    partie 1

--- script.js
+++
@@ -1,9 +0,0 @@
-var btn = document.querySelector('.btn');
-var body = document.querySelector('body');
-var audio = document.querySelector("#audio");
-
-
-btn.onclick = function() {
-  body.classList.toggle('on');
-  audio.play();
-}
