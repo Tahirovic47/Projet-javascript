eba4191dd2637c2dbeeb34e5e8c0096a12676d85
Tahirovic47 <162512072+Tahirovic47@users.noreply.github.com>
2024-04-07 08:31:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var btn = document.querySelector('.btn');
+var body = document.querySelector('body');
+var audio = document.querySelector("#audio");
+
+
+btn.onclick = function() {
+  body.classList.toggle('on');
+  audio.play();
+}
